e5809ce054e0ceaf4331c5f3b17f6c51ec5c4a93
MLGRussianXP <daniilkoles47@gmail.com>
2024-04-06 13:37:04 +0500

    Add .env support

--- foosh/foosh/settings.py
+++ foosh/foosh/settings.py
@@ -1,19 +1,28 @@
+from os import getenv
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+
+__all__ = []
+
+load_dotenv()
+
+
+def get_bool(name, default):
+    env_value = getenv(name, default).lower()
+    return env_value in ("true", "1", "y", "yes", "t", "")
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-iu*wi$_tgiaxj-_$!cg50k+!dzx&g%9at5-7tlq#i_yz&%h#ix"
-)
+SECRET_KEY = getenv("DJANGO_SECRET_KEY", "not_secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = get_bool("DJANGO_DEBUG", "False")
 
 ALLOWED_HOSTS = []
 
