cdc1fe039caab7dcb8c391b74ff6e71109f5738f
MLGRussianXP <daniilkoles47@gmail.com>
2024-04-08 19:45:10 +0500

    Create simple student and school models

--- foosh/users/models.py
+++ foosh/users/models.py
@@ -8,7 +8,19 @@ __all__ = []
 
 
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
+    email = models.EmailField(
+        unique=True,
+    )
+
+    is_student = models.BooleanField(
+        "ученик",
+        default=False,
+    )
+
+    is_school = models.BooleanField(
+        "школа",
+        default=False,
+    )
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
@@ -17,3 +29,49 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class School(models.Model):
+    user = models.OneToOneField(
+        CustomUser,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+    name = models.CharField(
+        "название",
+        max_length=255,
+        required=True,
+    )
+
+
+class Student(models.Model):
+    user = models.OneToOneField(
+        CustomUser,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+    name = models.CharField(
+        "имя",
+        max_length=100,
+        required=True,
+    )
+
+    surname = models.CharField(
+        "фамилия",
+        max_length=100,
+        required=True,
+    )
+
+    patronymic = models.CharField(
+        "отчество",
+        max_length=100,
+    )
+
+    school = models.ForeignKey(
+        School,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name="школа",
+    )
