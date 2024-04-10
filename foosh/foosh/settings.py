cf06124b2517d22b072d79953d2756ac2e335668
SSerov77 <sergo.sergej.05@yandex.ru>
2024-04-11 03:47:16 +0500

    fix default DEBUG value

--- foosh/foosh/settings.py
+++ foosh/foosh/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = getenv("DJANGO_SECRET_KEY", "not_secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = get_bool("DJANGO_DEBUG", "True")
+DEBUG = get_bool("DJANGO_DEBUG", "False")
 
 ALLOWED_HOSTS = []
 
