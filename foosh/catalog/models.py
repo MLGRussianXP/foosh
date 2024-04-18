03c3782de719a81fa84ce2926850ac17ffaa568c
MLGRussianXP <daniilkoles47@gmail.com>
2024-04-18 20:43:49 +0500

    Fix flake8

--- foosh/catalog/models.py
+++ foosh/catalog/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.utils.html import mark_safe
-from django.utils.translation import gettext_lazy as _
 from sorl.thumbnail import get_thumbnail, ImageField
 
 from users.models import School
